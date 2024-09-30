2aeb05172a595c5418cb92b84a757971a28c0f4a
Julien Semaan <jusemaa@akamai.com>
2024-09-30 17:23:48 +0000

    cont

--- hack/dis/setup-wireguard.sh
+++ hack/dis/setup-wireguard.sh
@@ -24,8 +24,9 @@ do
   idx=$((idx + 1))
   podcidr=$(kubectl get node $line -ocustom-columns=cidr:.spec.podCIDR --no-headers)
   ext_ip=$(kubectl get node $line --no-headers -owide | awk '{print $7}')
+  region=$(kubectl get node $line -ocustom-columns='region:.metadata.labels.topology\.kubernetes\.io/region' --no-headers)
 
-  info="$line $pvt_key $pub_key $wg_ip $podcidr $ext_ip"
+  info="$line $pvt_key $pub_key $wg_ip $podcidr $ext_ip $region"
 
   echo $pvt_key > $scratch
   scp $SCP_OPTS $scratch $ext_ip:/etc/wireguard/privatekey
@@ -42,15 +43,19 @@ done <<< "$(kubectl get nodes --no-headers | awk '{print $1}')"
 
 while read p; do
   ext_ip=$(echo $p | awk '{print $6}')
+  region1=$(echo $p | awk '{print $7}')
   while read p; do
     pub_key=$(echo $p | awk '{print $3}')
     wg_ip=$(echo $p | awk '{print $4}')
     podcidr=$(echo $p | awk '{print $5}')
     endpoint=$(echo $p | awk '{print $6}'):8172
+    region2=$(echo $p | awk '{print $7}')
     echo $pub_key $wg_ip $endpoint
     ssh $SSH_OPTS $ext_ip wg set wg0 peer $pub_key allowed-ips $wg_ip/32 allowed-ips $podcidr endpoint $endpoint
-    ssh $SSH_OPTS $ext_ip ip route del $podcidr || true
-    ssh $SSH_OPTS $ext_ip ip route add $podcidr via $wg_ip
+    if [ "$region1" != "$region2" ];then
+      ssh $SSH_OPTS $ext_ip ip route del $podcidr || true
+      ssh $SSH_OPTS $ext_ip ip route add $podcidr via $wg_ip
+    fi
   done <$peers_scratch
 done <$peers_scratch
 
